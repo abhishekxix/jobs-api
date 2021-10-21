0796ef6f9bbbd5a5a049e80d40d219d11467cd02
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-21 20:34:52 +0530

    add authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -10,4 +10,15 @@ const auth = async (req, res, next) => {
   }
 
   const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    // attach the user to the job route
+    req.user = { userId: payload.userId, name: payload.name }
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
 }
+
+module.exports = auth
--- routes/jobs.js
+++ routes/jobs.js
@@ -7,7 +7,6 @@ const {
   getJob,
 } = require('../controllers/jobs')
 const jobsRouter = express.Router()
-
 jobsRouter.route('/').get(getAllJobs).post(createJob)
 jobsRouter.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
 
