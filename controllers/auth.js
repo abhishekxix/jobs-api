1226892d18a509143aae2ba567459b0088b93d87
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-21 18:36:38 +0530

    Schema Instance Methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,10 @@
 const { StatusCodes } = require('http-status-codes')
 const User = require('../models/User')
-const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  const token = jwt.sign(
-    {
-      userId: user._id,
-      name: user.name,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: '30d',
-    }
-  )
+  const token = user.createJWT()
+
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
